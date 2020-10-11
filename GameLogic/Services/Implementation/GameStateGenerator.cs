e831b827a8c639df4f9e3beee04eb2ade6bc6f5d
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-10-11 14:35:37 +0300

    fix: GameStateGenerator.cs service

--- GameLogic/Services/Implementation/GameStateGenerator.cs
+++ GameLogic/Services/Implementation/GameStateGenerator.cs
@@ -1,36 +1,32 @@
 using System;
-using System.Reflection;
-using System.Linq;
-using WeirdUnitBE.GameLogic.TowerPackage;
-using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using WeirdUnitBE.GameLogic.PowerUpPackage;
 using WeirdUnitBE.GameLogic.PowerUpPackage.ConcreteCreators;
+using WeirdUnitBE.GameLogic.Services.Interfaces;
+using WeirdUnitBE.GameLogic.TowerPackage;
 
+// ReSharper disable once CheckNamespace
 namespace WeirdUnitBE.GameLogic.Services.Implementation
 {
-    public class GameStateGenerator
+    public class GameStateGenerator: IGameStateGenerator
     {
         private List<Tower> allTowerList = new List<Tower>();
         private List<PowerUp> allPowerUps = new List<PowerUp>();
 
-        private GameStateGenerator() { }
+        public GameStateGenerator() { }
 
-        public void GenerateRandomGameState(): GameState
+        public GameState GenerateRandomGameState()
         {
             GenerateUserTowers();
-            GenerateRandomTowers();
             GeneratePowerUps();
-
-            return new GameState(allTowerList, allPowerUps);
+            
+            return  new GameState(allTowerList, allPowerUps);
         }
 
         private void GeneratePowerUps()
         {
-            PowerUpCreator powerUpCreator = new PowerUpCreator();
-
-            powerUpCreator = new AttackingTowerPowerUpCreator();
+            PowerUpCreator powerUpCreator = new AttackingTowerPowerUpCreator();
             PowerUp powerUp = powerUpCreator.createPowerUp();
             allPowerUps.Add(powerUp);
 
@@ -43,7 +39,7 @@ namespace WeirdUnitBE.GameLogic.Services.Implementation
             allPowerUps.Add(powerUp);
         }
 
-        private void GenerateRandomTowers(AbstractFactory abstractTowerFactory)
+        private void GenerateRandomTowers()
         {
             int rTowerCount = Randomizer.ReturnRandomInteger(3, 6);
             for (int i = 0; i < rTowerCount; i++)
@@ -55,36 +51,34 @@ namespace WeirdUnitBE.GameLogic.Services.Implementation
                 }
 
                 // Generate Random tower
-                Tower tower = GenerateRandomTower(abstractTowerFactory);
+                Tower tower = GenerateRandomTower();
                 tower.unitCount = Randomizer.ReturnRandomInteger(0, 51);
                 tower.position = position;                
                 allTowerList.Add(tower); // cia
 
                 // Generate tower symmetric to the previous tower 
-                Tower newTower = tower.ReturnSymmetricTower(MAP_DIMENSIONS.X, MAP_DIMENSIONS.Y);
+                Tower newTower = tower.ReturnSymmetricTower(10, 10);
                 allTowerList.Add(newTower); // cia
             }
         }
 
         private Tower GenerateRandomTower()
         {
-            AbstractFactory abstractTowerFactory = new DefaultTowerFactory();
-
             Type type = Randomizer.ReturnRandomType<Tower>(); // Generate a random type
 
             IDictionary<Type, Tower> dictionary = new Dictionary<Type, Tower>();
 
-            abstractFactory = new DefaultTowerFactory();
-            dictionary.Add(typeof(DefaultAttackingTower), abstractFactory.CreateAttackingTower());
-            dictionary.Add(typeof(DefaultRegeneratingTower), abstractFactory.CreateRegeneratingTower());
+            AbstractFactory abstractTowerFactory = new DefaultTowerFactory();
+            dictionary.Add(typeof(DefaultAttackingTower), abstractTowerFactory.CreateAttackingTower());
+            dictionary.Add(typeof(DefaultRegeneratingTower), abstractTowerFactory.CreateRegeneratingTower());
 
-            abstractFactory = new FastTowerFactory();
-            dictionary.Add(typeof(FastAttackingTower), abstractFactory.CreateAttackingTower());
-            dictionary.Add(typeof(FastRegeneratingTower), abstractFactory.CreateRegeneratingTower());
+            abstractTowerFactory = new FastTowerFactory();
+            dictionary.Add(typeof(FastAttackingTower), abstractTowerFactory.CreateAttackingTower());
+            dictionary.Add(typeof(FastRegeneratingTower), abstractTowerFactory.CreateRegeneratingTower());
 
-            abstractFactory = new StrongTowerFactory();
-            dictionary.Add(typeof(StrongAttackingTower), abstractFactory.CreateAttackingTower());
-            dictionary.Add(typeof(StrongRegeneratingTower), abstractFactory.CreateRegeneratingTower());
+            abstractTowerFactory = new StrongTowerFactory();
+            dictionary.Add(typeof(StrongAttackingTower), abstractTowerFactory.CreateAttackingTower());
+            dictionary.Add(typeof(StrongRegeneratingTower), abstractTowerFactory.CreateRegeneratingTower());
 
             Tower randomTower = dictionary[type]; // Based on a generated random Tower type, create a new tower of that type
 
@@ -93,16 +87,14 @@ namespace WeirdUnitBE.GameLogic.Services.Implementation
 
         private void GenerateUserTowers()
         {
-            AbstractFactory abstractTowerFactory = new DefaultTowerFactory();
-
             // Generate First user tower
-            abstractFactory = new DefaultTowerFactory();
-            Tower initialUser1Tower = abstractFactory.CreateRegeneratingTower();
+            AbstractFactory abstractTowerFactory = new DefaultTowerFactory();
+            Tower initialUser1Tower = abstractTowerFactory.CreateRegeneratingTower();
             initialUser1Tower.position = new Position(0, 4);
             initialUser1Tower.unitCount = 50;
 
             // Generate Second users tower (which is symmetric to First user)
-            Tower initialUser2Tower = initialUser1Tower.ReturnSymmetricTower(MAP_DIMENSIONS.X, MAP_DIMENSIONS.Y);
+            Tower initialUser2Tower = initialUser1Tower.ReturnSymmetricTower(10, 10);
 
             // Store both towers su global List
             allTowerList.Add(initialUser1Tower);
@@ -110,8 +102,8 @@ namespace WeirdUnitBE.GameLogic.Services.Implementation
         }
         private Position GenerateRandomPosition()
         {
-            int x = Randomizer.ReturnRandomInteger(0, MAP_DIMENSIONS.X / 2);
-            int y = Randomizer.ReturnRandomInteger(0, MAP_DIMENSIONS.Y);
+            int x = Randomizer.ReturnRandomInteger(0, 10 / 2);
+            int y = Randomizer.ReturnRandomInteger(0, 10);
             return new Position(x, y);
         }
     }
