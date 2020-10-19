12ec7f5b9ce2278f5377174dee266d65b10de6c1
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-10-19 22:56:57 +0300

    feat: Added builder methods

--- GameLogic/Services/GameStateDirector.cs
+++ GameLogic/Services/GameStateDirector.cs
@@ -5,14 +5,31 @@ namespace WeirdUnitBE.GameLogic.Services
     public class GameStateDirector
     {
         GameState result = new GameState();
+
+        private IGameStateBuilder _gameStateBuilder; 
         
         public GameStateDirector(IGameStateBuilder gameStateBuilder, string user1, string user2)
         {
-            result.SetAllPowerUps(gameStateBuilder.GeneratePowerUps());
+            _gameStateBuilder = gameStateBuilder;
             
-            gameStateBuilder.GenerateUserTowers(user1,user2);
-            
-            result.PositionToTowerDict = gameStateBuilder.GenerateRandomTowers();
+            BuildPowerUps();
+            BuildUserTowers(user1, user2);
+            BuildRandomTowers();
+        }
+
+        public void BuildPowerUps()
+        {
+            result.SetAllPowerUps(_gameStateBuilder.GeneratePowerUps());
+        }
+
+        public void BuildUserTowers(string user1, string user2)
+        {
+            _gameStateBuilder.GenerateUserTowers(user1,user2);
+        }
+
+        public void BuildRandomTowers()
+        {
+            result.PositionToTowerDict = _gameStateBuilder.GenerateRandomTowers();
         }
 
         public GameState GetResult()
