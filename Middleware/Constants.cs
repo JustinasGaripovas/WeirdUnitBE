514cba54a84b4a67e9174e75cc1fb95cae06921c
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-11-09 23:27:17 +0200

    UNSTABLE

--- Middleware/Constants.cs
+++ Middleware/Constants.cs
@@ -12,13 +12,14 @@ public static class Constants
             public const string INITIAL = "s:Initial";
             public const string CONN_ID = "s:ConnID";
             public const string POWER_UP = "s:PowerUp";
+            public const string ARRIVED_TO = "s:ArrivedTo";
         }
 
         public static class ClientCommands
         {
-            public const string ARRIVED = "c:Arrived";
             public const string MOVE_TO = "c:MoveTo";
             public const string POWER_UP = "c:PowerUpClicked";
+            public const string ARRIVED_TO = "c:ArrivedTo";
 
             public const string ATTACKING_TOWER_POWER_UP = "AttackingTowerPowerUp";
             public const string REGENERATING_TOWER_POWER_UP = "RegeneratingTowerPowerUp";
