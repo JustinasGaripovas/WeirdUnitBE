7c5f6c2ff7157cec7340f8828e4f677c9019f40e
315salzaz <315salzaz@gmail.com>
2020-11-27 00:21:17 +0200

    feat: upgrade executive can now generate each tower type

--- GameLogic/Services/Implementation/UpgradeTowerExecutive.cs
+++ GameLogic/Services/Implementation/UpgradeTowerExecutive.cs
@@ -31,22 +31,43 @@ namespace WeirdUnitBE.GameLogic
             newTower.owner = tower.owner;
             newTower.neighbourTowers = tower.neighbourTowers;
 
-            tower = newTower;         
+            tower = newTower;
         }
 
         private static Tower CreateEmptyTowerWithType(string upgradableTowerType)
         {
-            AbstractFactory towerFactory = new DefaultTowerFactory();
-            
-            if(String.Compare(upgradableTowerType, "Regenerating") == 0) {
-                return towerFactory.CreateRegeneratingTower();  
+            AbstractFactory defaultFactory = new DefaultTowerFactory();
+            AbstractFactory fastFactory = new FastTowerFactory();
+            AbstractFactory strongFactory = new StrongTowerFactory();
+
+            if (String.Compare(upgradableTowerType, "DefaultRegeneratingTower") == 0)
+            {
+                return defaultFactory.CreateRegeneratingTower();
             }
-            else if(String.Compare(upgradableTowerType, "Attacking") == 0) {
-                return towerFactory.CreateAttackingTower();
+            else if (String.Compare(upgradableTowerType, "DefaultAttackingTower") == 0)
+            {
+                return defaultFactory.CreateAttackingTower();
             }
-            else {
+            else if (String.Compare(upgradableTowerType, "FastRegeneratingTower") == 0)
+            {
+                return fastFactory.CreateRegeneratingTower();
+            }
+            else if (String.Compare(upgradableTowerType, "FastAttackingTower") == 0)
+            {
+                return fastFactory.CreateAttackingTower();
+            }
+            else if (String.Compare(upgradableTowerType, "StrongRegeneratingTower") == 0)
+            {
+                return strongFactory.CreateRegeneratingTower();
+            }
+            else if (String.Compare(upgradableTowerType, "StrongAttackingTower") == 0)
+            {
+                return strongFactory.CreateAttackingTower();
+            }
+            else
+            {
                 throw new InvalidUpgradeException("Cannot resolve Upgrade type");
-            }           
+            }
         }
 
         private static object FormatCommand(Tower tower)
