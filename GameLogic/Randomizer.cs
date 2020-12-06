63f44c40933d9912e88c772da000b2ebe2aa8d07
Justin <Justin@DESKTOP-FM1CJC1>
2020-12-06 19:55:45 +0200

    fix:randomizer

--- GameLogic/Randomizer.cs
+++ GameLogic/Randomizer.cs
@@ -10,9 +10,9 @@ namespace WeirdUnitBE.GameLogic
     public class Randomizer
     {
         private int _seed = DateTime.Now.Millisecond;
-        public int ReturnRandomInteger(int from, int to)
+        public int ReturnRandomInteger(int from, int to, int? seed = null)
         {
-            Random random = new Random(_seed);
+            Random random = new Random(seed??_seed);
             _seed += 100;
             int rInteger = random.Next(from, to);
             return rInteger;
